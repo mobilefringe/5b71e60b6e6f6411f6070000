a90b2638bc183c5bc9b16bce7dc8dd6765d81b0a
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-08-20 17:11:16 -0400

    Pacific Commons 2.0 1.0  /center_information.vue

--- center_information.vue
+++ center_information.vue
@@ -69,9 +69,9 @@
             },
             created() {
                 this.loadData().then(response => {
-                    var temp_repo = this.findRepoByName('Center Information Banner').images;
-                    if(temp_repo != null) {
-                        this.pageBanner = temp_repo[0];
+                    var repo = this.findRepoByName('Center Information Banner').images;
+                    if(repo != null) {
+                        this.pageBanner = repo[0];
                     } else {
                         this.pageBanner = {
                             "image_url": "//codecloud.cdn.speedyrails.net/sites/5b71e60b6e6f6411f6070000/image/jpeg/1529532304000/insidebanner2.jpg"
