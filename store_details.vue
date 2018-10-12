a20d958b9306776b53bda1e8c5ffcdff33db7887
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-10-12 16:18:32 -0400

    Pacific Commons 2.0 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -200,7 +200,7 @@
                         var current_promo = vm.findPromoById(value);
                         
                         if (_.includes(current_promo.image_url, 'missing')) {
-                            current_promo.image_url = "//codecloud.cdn.speedyrails.net/sites/5b71e60b6e6f6411f6070000/image/png/1529532181000/promoplaceholder2@2x.png";
+                            current_promo.image_url = "//codecloud.cdn.speedyrails.net/sites/5b71e60b6e6f6411f6070000/image/png/1539375270000/pc_promo_default.png";
                         }
 
                         temp_promo.push(current_promo);
