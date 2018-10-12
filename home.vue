9f2e54e9a41c132848b39f6cef3f2fd78efcd189
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-10-12 15:35:36 -0400

    Pacific Commons 2.0 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -6,29 +6,32 @@
                 <div class="home_banner_container">
                     <div class="prev"></div>
                     <slick ref="slick" :options="slickOptions">
-                        <div v-if="homeBanners" v-for="banner in homeBanners">
-                            <div v-if="banner.name && banner.description" class="banner_height">
-                                <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>
-                                <div class="banner_content_container">
-                                    <div class="banner_content">
-                                        <span v-if="banner.heading" class="banner_heading">{{ banner.heading }}</span>
-                                        <h1 class="banner_title">{{ banner.name }}</h1>
-                                        <p class="banner_text">{{ banner.description }}</p>
-                                        <a :href="banner.url">
-                                            <span class="banner_btn animated_btn">Find Out More</span>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div v-else-if="!banner.url">
-                                <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>
-                            </div>
-                            <div v-else>
-                                <a :href="banner.url">
-                                    <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>
-                                </a>
-                            </div>
-                        </div>
+                        <!-- DYNAMIC BANNERS -->
+                        <!--<div v-if="homeBanners" v-for="banner in homeBanners">-->
+                        <!--    <div v-if="banner.name && banner.description && banner.url" class="banner_height">-->
+                        <!--        <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>-->
+                        <!--        <div class="banner_content_container">-->
+                        <!--            <div class="banner_content">-->
+                        <!--                <span v-if="banner.heading" class="banner_heading">{{ banner.heading }}</span>-->
+                        <!--                <h1 class="banner_title">{{ banner.name }}</h1>-->
+                        <!--                <p class="banner_text">{{ banner.description }}</p>-->
+                        <!--                <a :href="banner.url">-->
+                        <!--                    <span class="banner_btn animated_btn">Find Out More</span>-->
+                        <!--                </a>-->
+                        <!--            </div>-->
+                        <!--        </div>-->
+                        <!--    </div>-->
+                        <!--    <div v-else-if="!banner.url">-->
+                        <!--        <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>-->
+                        <!--    </div>-->
+                        <!--    <div v-else>-->
+                        <!--        <a :href="banner.url">-->
+                        <!--            <div class="banner_image" v-bind:style="{ backgroundImage: 'url(' + banner.image_url + ')' }"></div>-->
+                        <!--        </a>-->
+                        <!--    </div>-->
+                        <!--</div>-->
+
+                        <div class="banner_image" v-bind:style="{ backgroundImage: 'url(//codecloud.cdn.speedyrails.net/sites/5b71e60b6e6f6411f6070000/image/jpeg/1539372793000/pc_home_1925x470-compressor.jpg)' }"></div>
                     </slick>
                     <div class="next"></div>
                 </div>
