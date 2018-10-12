e8875e5e103e4ab2ddc636b1bdc667f69fc685b8
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-10-12 15:40:00 -0400

    Pacific Commons 2.0 1.0  /main.js

--- main.js
+++ main.js
@@ -128,6 +128,10 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'vuex-router-syn
                                 href: "/contact-us"
                             },
                             {
+                                name: "Leasing",
+                                href: "/leasing"
+                            },
+                            {
                                 name: "Newsletter Sign Up",
                                 href: "/newsletter"
                             }
