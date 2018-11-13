fa26716912f242fac43bdd357c8f7904639ad413
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2018-11-13 14:35:51 -0500

    Pacific Commons 2.0 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -32,6 +32,8 @@
                         <!--</div>-->
 
                         <div class="banner_image" v-bind:style="{ backgroundImage: 'url(//codecloud.cdn.speedyrails.net/sites/5b71e60b6e6f6411f6070000/image/jpeg/1539372793000/pc_home_1925x470-compressor.jpg)' }"></div>
+<div class="banner_image" v-bind:style="{ backgroundImage: 'url(//codecloud.cdn.speedyrails.net/sites/5b71e60b6e6f6411f6070000/image/jpeg/1539372793000/pc_home_1925x470-compressor.jpg)' }"></div>
+
                     </slick>
                     <div class="next"></div>
                 </div>
