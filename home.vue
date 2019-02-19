03cc7f81a81376c0ce6f586d5752b336a03c5377
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-02-19 15:18:17 -0500

    Pacific Commons 2.0 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -195,7 +195,7 @@
                     var temp_image_urls = ["//codecloud.cdn.speedyrails.net/sites/5b71e60b6e6f6411f6070000/image/jpeg/1550603793000/PC_HungryTreat119_images2a.jpg","//codecloud.cdn.speedyrails.net/sites/5b71e60b6e6f6411f6070000/image/jpeg/1550603793000/PC_HungryTreat119_images2b.jpg", "//codecloud.cdn.speedyrails.net/sites/5b71e60b6e6f6411f6070000/image/jpeg/1539372793000/pc_home_1925x470-compressor.jpg","//codecloud.cdn.speedyrails.net/sites/5b71e60b6e6f6411f6070000/image/jpeg/1542071972000/PC_instagram1118_1925x470_1.jpg","//codecloud.cdn.speedyrails.net/sites/5b71e60b6e6f6411f6070000/image/jpeg/1542071964000/PC_generic1118_1925x470_1.jpg"];
                     var temp_names = ["Treat Yourself","HUNGRY?", null, null, null];
                     var temp_desc = ["You deserve it.","We know just the place...", null,null,null];
-                    var temp_urls = [];
+                    var temp_urls = ["/stores", "/dining", null,null,null];
                     
                     _.forEach(this.$store.state.banners, function (value, key) {
                         var today = new Date();
