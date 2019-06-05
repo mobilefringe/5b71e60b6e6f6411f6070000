d9b5237e15f90b418178680807f19577a8fdc2ab
stephanie@mobilefringe.com @ CodeCloud <stephanie@mobilefringe.com @ CodeCloud>
2019-06-05 12:29:36 -0400

    Pacific Commons 2.0 1.0  /specialty_leasing.vue

--- specialty_leasing.vue
+++ specialty_leasing.vue
@@ -55,7 +55,7 @@
             },
             created() {
                 this.loadData().then(response => {
-                    var temp_repo = this.findRepoByName('Specialty Leasing Banner').images;
+                    var temp_repo = this.findRepoByName('Leasing Banner').images;
                     if(temp_repo != null) {
                         this.pageBanner = temp_repo[0];
                     } else {
