736485d9d1404625c082738a10cfd51b1b32697f
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-25 11:13:51 -0500

    Pacific Commons 2.0 1.2  /delivery.vue

--- delivery.vue
+++ delivery.vue
@@ -54,30 +54,30 @@
                                 <div v-masonry-tile  v-for="(store, index) in filteredStores" :key="index" class="stores-grid-item">
                             	    <div class="store_logo_container">
                             	        <router-link :to="'/stores/'+ store.slug">
-                                			<div v-if="!store.no_store_logo">
-                                			    <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png" alt="">
-                                			    <img class="store_img" :src="store.store_front_url_abs" alt="">
-                                			</div>
-                                			
+                                            <div v-if="!store.no_store_logo">
+                                                <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5ddb449e6e6f6416db040000/image/png/1536094188000/default_background.png" alt="">
+                                                <img  class="store_img" :src="store.store_front_url_abs" :alt="store.name + 'Logo'">
+                                            </div>
+                                            
                                             <div v-else class="no_logo_container">
-                                                <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png" alt="No Logo">
+                                                <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5ddb449e6e6f6416db040000/image/png/1536094188000/default_background.png" alt="">
                                                 <div class="no_logo_text">
                                                     <div class="store_text"><h4>{{ store.name }}</h4></div>
                                                 </div>
                                             </div>
-                                			<div class="store_tag" v-if="store.total_published_promos">
-            									<div class="store_tag_text">Promotion</div>
-            								</div>
-            								<div class="store_tag" v-if="!store.total_published_promos && store.is_coming_soon_store">
-            									<div class="store_tag_text">Coming Soon</div>
-            								</div>
-            								<div class="store_tag" v-if="!store.total_published_promos && !store.is_coming_soon_store && store.is_new_store">
-            									<div class="store_tag_text">New Store</div>
-            								</div>
-            								<div class="store_details">
-            								    <div class="store_text"><h4>{{ store.name }}</h4></div>    
-            								</div>
-                                		</router-link>
+                                            <div class="store_tag" v-if="store.total_published_promos">
+                                                <div class="store_tag_text">Promotion</div>
+                                            </div>
+                                            <div class="store_tag" v-if="!store.total_published_promos && !store.is_new_store && store.is_coming_soon_store">
+                                                <div class="store_tag_text">Coming Soon</div>
+                                            </div>
+                                            <div class="store_tag" v-if="!store.total_published_promos && !store.is_coming_soon_store && store.is_new_store">
+                                                <div class="store_tag_text">New Store</div>
+                                            </div>
+                                            <div class="store_details">
+                                                <div class="store_text"><h4>{{ store.name }}</h4></div>    
+                                            </div>
+                                        </router-link>
                             	    </div>
                                 </div>
                             </transition-group>
