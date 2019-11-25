e52504531c0f736d38e0e8fb754bb9e8d2da3e3c
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-25 11:17:56 -0500

    Pacific Commons 2.0 1.2  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -19,7 +19,7 @@
                             <div class="sidebar">
                                 <div v-if="currentStore.no_logo" class="store_details_no_logo center-block">
                                     <div class="no_logo">
-                                        <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png">
+                                        <img class="transparent_logo" src="//codecloud.cdn.speedyrails.net/sites/5b1550796e6f641cab010000/image/png/1536094421888/default_background.png" alt="">
                                         <p class="store_details_name">{{ currentStore.name }}</p>
                                     </div>    
                                 </div>
@@ -182,7 +182,6 @@
                     }
                 
                     this.updateCurrentStore(this.id);
-                    console.log(this.currentStore)
                     this.getSVGMap;
                     this.dataLoaded = true;
                 });
